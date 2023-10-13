a517750086fba8f5d8d72b89ecaa1c7fecabf0a5
Rizajin <jarkko.vaisanen@live.fi>
2023-10-14 01:33:55 +0300

    both sides, Rust as executable user and C++ as a dll(win only demo)

--- cpp-lib-2/src/lib.cpp
+++ cpp-lib-2/src/lib.cpp
@@ -5,11 +5,41 @@
 #endif
 
 #include <cstdint>
+#include <cstring>
 
-// some stuff to send cross
+// some stuff to send cross (as in rust side)
 struct Data
 {
     const char* d;
     int32_t length;
 };
 
+extern "C" 
+{
+    COOLEST_API Data get_data();
+    COOLEST_API void free_data(Data d);
+}
+
+// what ever you want "block" just, lets use our imagination. given we have one.
+
+extern "C"
+{
+    Data get_data()
+    {
+        const char* raw_data = "I love C++! Maybe!";
+        // lets allocate a some
+        char* copy = new char[strlen(raw_data) + 1];
+        strcpy(copy,raw_data);
+
+        Data d;
+        d.d = copy;
+        d.length = strlen(copy);
+
+        return d;
+    }
+
+    void free_data(Data d)
+    {
+        delete[] d.d; // pun accident?
+    }
+}
--- rust-user-2/src/main.rs
+++ rust-user-2/src/main.rs
@@ -1,4 +1,5 @@
 use std::ffi::CStr;
+use std::slice;
 
 // some stuff to get cross (as in c++ side)
 #[repr(C)]
@@ -7,6 +8,20 @@ struct Data{
     length: i32,
 }
 
+extern "C" {
+    fn get_data() -> Data;
+    fn free_data(d: Data);
+}
+
 fn main() {
-    println!("Hello, world!");
+    unsafe{ // yes.. I dont feel safe.
+        let data = get_data(); // <- ownership comes here.
+        let data_slice = slice::from_raw_parts(data.d as *const u8, data.length as usize);
+        let this_is_the_str_you_are_looking_for = CStr::from_bytes_with_nul_unchecked(data_slice).to_str().unwrap();
+        // lets print the what ever?
+        println!("{}", this_is_the_str_you_are_looking_for);
+
+        // remember, it was not made with Rust so lets ensure the outside allocation gets free'd.
+        free_data(data);
+    }
 }
